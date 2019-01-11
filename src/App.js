ea30dd162e98c9a4732530a4379d9f3fed094370
Daisuke Akiyama <dai.akiyam@gmail.com>
2019-01-11 18:27:30 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            See you later
           </a>
         </header>
       </div>
